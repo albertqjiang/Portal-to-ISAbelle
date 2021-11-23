fd0fc32d80d14895ecdcf8f4e733c3d460ece2ee
Albert Jiang <albert594250@gmail.com>
2021-11-23 13:06:11 +0800

    calculate retainment rate

--- scripts/length_in_char_stats.py
+++ scripts/length_in_char_stats.py
@@ -10,8 +10,11 @@ if __name__ == "__main__":
     with open(args.path_to_proof_and_state_file) as fin:
         for line in fin.readlines():
             lengths.append(len(line.strip()))
-    
+            
+    maximum_sequence_length_in_chars = 256 * 3
+
     print(f"Minimum: {min(lengths)}")
     print(f"Maximum: {max(lengths)}")
     print(f"Average: {sum(lengths)/len(lengths)}")
     print(f"Median: {sorted(lengths)[int(len(lengths)/2)]}")
+    print(f"Retainment rate: {sorted(lengths + [maximum_sequence_length_in_chars]).index(maximum_sequence_length_in_chars) / (len(lengths)+1)}")
