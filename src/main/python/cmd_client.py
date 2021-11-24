b361b6706da4d367f847d88bc971efc0c3098228
Albert Jiang <albert594250@gmail.com>
2021-11-24 12:19:02 +0800

    debug

--- src/main/python/cmd_client.py
+++ src/main/python/cmd_client.py
@@ -5,13 +5,13 @@ import os
 
 if __name__ == "__main__":
     isa_path = input("Path to Isabelle (default /home/qj213/Isabelle2021): ")
-    isa_path = "/home/qj213/Isabelle2021" if isa_path is None else isa_path
+    isa_path = "/home/qj213/Isabelle2021" if not isa_path else isa_path.strip()
     afp_path = input("Path to AFP (default /home/qj213/afp-2021-10-22): ")
-    afp_path = "/home/qj213/afp-2021-10-22" if afp_path is None else afp_path
+    afp_path = "/home/qj213/afp-2021-10-22" if not afp_path else afp_path.strip()
     file_path = input("Path to an AFP file (default /home/qj213/afp-2021-10-22/FunWithFunctions/FunWithFunctions.thy): ")
-    file_path = "/home/qj213/afp-2021-10-22/FunWithFunctions/FunWithFunctions.thy" if file_path is None else file_path
+    file_path = "/home/qj213/afp-2021-10-22/FunWithFunctions/FunWithFunctions.thy" if not file_path else file_path.strip()
     env = IsaFlexEnv(
-        port=8000, isa_path=isa_path, starter_string="theory Test imports Complex_Main begin",
+        port=8000, isa_path=isa_path, starter_string=file_path,
         working_directory=os.path.join(afp_path, "thys", "FunWithFunctions")     
     )
 
