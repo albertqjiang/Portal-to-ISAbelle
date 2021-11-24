7ecd66e89b7ec0d453f3a9b1f2d010fab3f5c2c3
Albert Jiang <albert594250@gmail.com>
2021-11-24 12:57:28 +0800

    debug

--- src/main/python/cmd_client.py
+++ src/main/python/cmd_client.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     isa_path = "/home/qj213/Isabelle2021" if not isa_path else isa_path.strip()
     afp_path = input("Path to AFP (default /home/qj213/afp-2021-10-22): ")
     afp_path = "/home/qj213/afp-2021-10-22" if not afp_path else afp_path.strip()
-    file_path = input("Path to an AFP file (default /home/qj213/afp-2021-10-22/FunWithFunctions/FunWithFunctions.thy): ")
-    file_path = "/home/qj213/afp-2021-10-22/FunWithFunctions/FunWithFunctions.thy" if not file_path else file_path.strip()
+    file_path = input("Path to an AFP file (default /home/qj213/afp-2021-10-22/thys/FunWithFunctions/FunWithFunctions.thy): ")
+    file_path = "/home/qj213/afp-2021-10-22/thys/FunWithFunctions/FunWithFunctions.thy" if not file_path else file_path.strip()
     env = IsaFlexEnv(
         port=8000, isa_path=isa_path, starter_string=file_path,
         working_directory=os.path.join(afp_path, "thys", "FunWithFunctions")     
