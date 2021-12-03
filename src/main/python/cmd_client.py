edd084555738088abdd7ffd537c25c510c32d450
Albert Jiang <qj213@cam.ac.uk>
2021-12-03 18:53:13 +0800

    debug

--- src/main/python/cmd_client.py
+++ src/main/python/cmd_client.py
@@ -6,13 +6,13 @@ import os
 if __name__ == "__main__":
     isa_path = input("Path to Isabelle (default /home/qj213/Isabelle2021): ")
     isa_path = "/home/qj213/Isabelle2021" if not isa_path else isa_path.strip()
-    afp_path = input("Path to AFP (default /home/qj213/afp-2021-10-22): ")
-    afp_path = "/home/qj213/afp-2021-10-22" if not afp_path else afp_path.strip()
-    file_path = input("Path to an AFP file (default /home/qj213/afp-2021-10-22/thys/FunWithFunctions/FunWithFunctions.thy): ")
-    file_path = "/home/qj213/afp-2021-10-22/thys/FunWithFunctions/FunWithFunctions.thy" if not file_path else file_path.strip()
+    afp_path = input("Path to the working directory (default /home/qj213/Isabelle2021/src/HOL): ")
+    afp_path = "/home/qj213/Isabelle2021/src/HOL" if not afp_path else afp_path.strip()
+    file_path = input("Path to a theory file (default /home/qj213/Isabelle2021/src/HOL/HOL.thy): ")
+    file_path = "/home/qj213/Isabelle2021/src/HOL/HOL.thy" if not file_path else file_path.strip()
     env = IsaFlexEnv(
         port=8000, isa_path=isa_path, starter_string=file_path,
-        working_directory=os.path.join(afp_path, "thys", "FunWithFunctions")     
+        working_directory=afp_path,
     )
 
     while True:
