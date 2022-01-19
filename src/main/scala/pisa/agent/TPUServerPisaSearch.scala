a630ced5f90fb5ae462dc52a07fc4f6cc69db8c7
Albert Jiang <qj213@cam.ac.uk>
2022-01-19 12:31:28 +0000

    debug

--- src/main/scala/pisa/agent/TPUServerPisaSearch.scala
+++ src/main/scala/pisa/agent/TPUServerPisaSearch.scala
@@ -214,33 +214,31 @@ class TPUPisaSearch(use_proof: Boolean = false, use_conjecture: Boolean = false,
         val candidate_commands_and_logprobs = coordinate_and_make_texts_and_logprobs_distinct(candidate_commands, candidate_logprobs)
         var proceed : Boolean = false
         
-
-        Breaks.breakable{
-          for (i <- List.range(0, candidate_commands_and_logprobs.length)) {
-            if (proceed) {}
-            else {
-              val proof_command = process_string(candidate_commands_and_logprobs(i)._1)
-            
-              try {
-                val child_toplevel = pisaos.step(proof_command, ToplevelState.instantiate(toplevel.mlValue))
-                toplevel = child_toplevel
-                proof_till_now = proof_till_now + "\n" + proof_command
-                proceed = true
-                if (pisaos.proof_level(toplevel).retrieveNow == 0) {
-                  proved = true
-                  index_to_successful_skeletons(0) = proof_till_now
-                }
-              } catch {
-                case _: Throwable =>
+        for (i <- List.range(0, candidate_commands_and_logprobs.length)) {
+          if (proceed) {}
+          else {
+            val proof_command = process_string(candidate_commands_and_logprobs(i)._1)
+          
+            try {
+              val child_toplevel = pisaos.step(proof_command, ToplevelState.instantiate(toplevel.mlValue))
+              toplevel = child_toplevel
+              proof_till_now = proof_till_now + "\n" + proof_command
+              proceed = true
+              if (pisaos.proof_level(toplevel).retrieveNow == 0) {
+                proved = true
+                index_to_successful_skeletons(0) = proof_till_now
               }
+            } catch {
+              case _: Throwable =>
             }
           }
-          if (proved) return (1, "Proved", proof_till_now, -1, index_to_successful_skeletons.toMap)
-          if (!proceed) {
-            index_to_successful_skeletons(-1) = "Empty"
-            return (0, "Queue empty", "", -1, index_to_successful_skeletons.toMap)
-          }
         }
+        if (proved) return (1, "Proved", proof_till_now, -1, index_to_successful_skeletons.toMap)
+        // if (!proceed) {
+        //   index_to_successful_skeletons(-1) = "Empty"
+        //   return (0, "Queue empty", "", -1, index_to_successful_skeletons.toMap)
+        // }
+      
       }
     }
     index_to_successful_skeletons(-1) = "Empty"
