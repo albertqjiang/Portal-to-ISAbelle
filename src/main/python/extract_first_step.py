e30be42004f3c9c54f72e369dfba8fafc45dbaec
Albert Jiang <qj213@cam.ac.uk>
2022-02-09 22:13:43 +0000

    debug

--- src/main/python/extract_first_step.py
+++ src/main/python/extract_first_step.py
@@ -8,12 +8,18 @@ first_step_dir = "/home/qj213/first_step"
 
 
 for file in os.listdir(proof_and_state_dir):
-    with open(os.path.join(proof_and_state_dir, file)) as fhand, open(os.path.join(first_step_dir, file), "w") as fout:
+    split_name = file.strip().strip(".json").strip()
+    with open(os.path.join(proof_and_state_dir, file)) as fhand, \
+        open(os.path.join(first_step_dir, f"{split_name}.src"), "w") as src_out, \
+            open(os.path.join(first_step_dir, f"{split_name}.tgt"), "w") as tgt_out:
         for line in tqdm(fhand.readlines()):
             line_json = json.loads(line.strip())
             source = line_json["source"]
             proof_step_string = source.split("<PS_SEP>")[0].strip()
+            proof_state_string = source.split("<PS_SEP>")[1].strip()
+            target = line_json["target"]
             if "\\n" not in proof_step_string:
                 # This is the first step
-                fout.write(line.strip() + "\n")
+                src_out.write(f"<ISA_OBS> {proof_state_string}\n")
+                tgt_out.write(f"{target}\n")
 
