ed5312ed22a3d3b68f06a03553facfb34f1b1f78
Albert Jiang <qj213@cam.ac.uk>
2022-04-07 17:41:13 +0100

    added ports

--- src/main/scala/pisa/server/PisaOneStageServers.scala
+++ src/main/scala/pisa/server/PisaOneStageServers.scala
@@ -26,3 +26,28 @@ object PisaOneStageServer12000 extends ServerMain {
   override def port : Int = 12000
   def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
 }
+
+object PisaOneStageServer13000 extends ServerMain {
+  override def port : Int = 13000
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
+}
+
+object PisaOneStageServer14000 extends ServerMain {
+  override def port : Int = 14000
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
+}
+
+object PisaOneStageServer15000 extends ServerMain {
+  override def port : Int = 15000
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
+}
+
+object PisaOneStageServer16000 extends ServerMain {
+  override def port : Int = 16000
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
+}
+
+object PisaOneStageServer17000 extends ServerMain {
+  override def port : Int = 17000
+  def services: ServiceList[zio.ZEnv] = ServiceList.add(new OneStageBody)
+}
