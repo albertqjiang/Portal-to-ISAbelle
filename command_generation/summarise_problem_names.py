508146b70a610935a3ba93c667cadbdc165b050b
Albert Jiang <qj213@cam.ac.uk>
2022-04-08 11:20:32 +0100

    added split

--- command_generation/summarise_problem_names.py
+++ command_generation/summarise_problem_names.py
@@ -17,27 +17,29 @@ if __name__ == "__main__":
     os.mkdir(dump_path)
 
     total = 0
-    for entry in os.listdir(args.extraction_path):
-        entry_path = os.path.join(args.extraction_path, entry)
-        for ground_truth_file in os.listdir(entry_path):
-            if "ground_truth" in ground_truth_file:
-                ground_truth_path = os.path.join(entry_path, ground_truth_file)
-                ground_truth = json.load(open(ground_truth_path))
-                file_name = ground_truth["file_name"]
-                problem_names = ground_truth["problem_names"]
-                assert len(problem_names) == 1
-                theorem_name = problem_names[0]
-                theorem_name = theorem_name.replace("\n", " ")
-                theorem_name = " ".join(theorem_name.split())
-                saving_name = file_name.split("/")[-1].strip(".thy")
-                json.dump(
-                    [
+    for split in os.listdir(args.extraction_path):
+        split_path = os.path.join(args.extraction_path, split)
+        for entry in os.listdir(split_path):
+            entry_path = os.path.join(split_path, entry)
+            for ground_truth_file in os.listdir(entry_path):
+                if "ground_truth" in ground_truth_file:
+                    ground_truth_path = os.path.join(entry_path, ground_truth_file)
+                    ground_truth = json.load(open(ground_truth_path))
+                    file_name = ground_truth["file_name"]
+                    problem_names = ground_truth["problem_names"]
+                    assert len(problem_names) == 1
+                    theorem_name = problem_names[0]
+                    theorem_name = theorem_name.replace("\n", " ")
+                    theorem_name = " ".join(theorem_name.split())
+                    saving_name = file_name.split("/")[-1].strip(".thy")
+                    json.dump(
                         [
-                            file_name, theorem_name
-                        ]
-                    ],
-                    open(
-                        os.path.join(dump_path, f"test_name_{total}.json"), "w"
+                            [
+                                file_name, theorem_name
+                            ]
+                        ],
+                        open(
+                            os.path.join(dump_path, f"test_name_{total}.json"), "w"
+                        )
                     )
-                )
-                total += 1
+                    total += 1
