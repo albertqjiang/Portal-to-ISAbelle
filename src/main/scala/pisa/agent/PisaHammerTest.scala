05d8f05573ecb8795ce5be09878c9ca3bb14375c
Albert Jiang <qj213@cam.ac.uk>
2022-04-16 22:14:40 +0100

    added script for calling hammer

--- src/main/scala/pisa/agent/PisaHammerTest.scala
+++ src/main/scala/pisa/agent/PisaHammerTest.scala
@@ -11,6 +11,29 @@ import scala.io.Source
 object PisaHammerTest {
   implicit val formats: DefaultFormats = DefaultFormats
 
+  def get_proved(pisaos: PisaOS) : Boolean = {
+    val hammer_results = pisaos.prove_with_hammer(pisaos.toplevel)
+    val hammered_string =
+      if (hammer_results._1) {
+        val hammer_strings = hammer_results._2
+        var found = false
+        var real_string = ""
+        for (attempt_string <- hammer_strings) {
+          if (!found && (attempt_string contains "Try this:")) {
+            found = true
+            real_string = attempt_string.trim.stripPrefix("Try this:").trim.split('(').dropRight(1).mkString("(")
+          }
+        }
+        if (found) real_string
+        else throw IsabelleException("Hammered said it worked but didn't find proof.")
+      } else {
+        throw IsabelleException("Hammer failed")
+      }
+
+    val new_state = pisaos.step(hammered_string, pisaos.toplevel, timeout_in_millis = 10000)
+    pisaos.getProofLevel(new_state) == 0
+  }
+
   def main(args: Array[String]): Unit = {
     val test_theorem_number: String = args(0).split('/').last.split('.').head.split('_').last
     val dump_path: String = "results/hammer_eval"
@@ -26,9 +49,9 @@ object PisaHammerTest {
     )
     pisaos.step_to_transition_text(theorem_name)
 
-    val proved =
+    val proved : Boolean =
       try {
-        pisaos.check_if_provable_with_Sledgehammer()
+        get_proved(pisaos)
       } catch {
         case _: IsabelleException => false
         case _: TimeoutException => false
