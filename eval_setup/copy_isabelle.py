14a24915e53fdf881a5d3e30fc35bdea10997aac
albertqjiang <aqj@fb.com>
2022-10-05 08:07:27 -0700

    debug

--- eval_setup/copy_isabelle.py
+++ eval_setup/copy_isabelle.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
     parser.add_argument("--output-path", type=str, required=True)
     args = parser.parse_args()
 
+    isabelle_identifier = args.isabelle.split("/")
+    isabelle_identifier = isabelle_identifier[-1] if isabelle_identifier[-1] else isabelle_identifier[-2]
+
     for index in tqdm(range(args.number_of_copies)):
         index_path = os.path.join(args.output_path, f"isabelle_copy_{index}")
         if not os.path.exists(index_path):
@@ -20,20 +23,17 @@ if __name__ == "__main__":
         main_isa_path = os.path.join(index_path, "main_isa")
         if not os.path.exists(main_isa_path):
             os.makedirs(main_isa_path)
-        shutil.copytree(args.isabelle, main_isa_path)
+        shutil.copytree(args.isabelle, os.path.join(main_isa_path, isabelle_identifier))
 
         user_isa_path = os.path.join(index_path, "user_isa")
-        if not os.path.exists(user_isa_path):
-            os.makedirs(user_isa_path)
+        if os.path.exists(user_isa_path):
+            raise AssertionError
         shutil.copytree(args.isabelle_user, user_isa_path)
 
         # Edit the settings file such that the user home points to the right directory
         original_isabelle_home_user_string = "$USER_HOME/.isabelle"
         isabelle_home_user_string = str(user_isa_path)
         
-        isabelle_versions = list(os.listdir(main_isa_path))
-        assert len(isabelle_versions) == 1
-        isabelle_identifier = isabelle_versions[0]
         isabelle_settings_path = os.path.join(main_isa_path, isabelle_identifier, "etc/settings")
         with open(isabelle_settings_path, "r") as f:
             settings = f.read()
