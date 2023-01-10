82568da261a9c9dfffaeee1f1c0d399aa23df52f
Albert Jiang <qj213@cam.ac.uk>
2023-01-10 17:13:00 +0000

    d

--- src/main/scala/pisa/server/PisaOS.scala
+++ src/main/scala/pisa/server/PisaOS.scala
@@ -737,7 +737,7 @@ class PisaOS(
       println(text)
       println(sanitised_theorem_name)
       println("*******")
-      Thread.sleep(5000)
+      Thread.sleep(1000)
       val sanitised_text = text.trim.replaceAll("\n", " ").replaceAll(" +", " ")
       if (sanitised_text == sanitised_theorem_name){
         found_theorem = true
